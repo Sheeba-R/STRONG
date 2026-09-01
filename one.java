9469f5ed354dd3bf44022fed370db7dc6782685e
unknown <sheebarajaram123@gmail.com>
2026-09-01 12:09:21 +0530

    added

---
+++ one.java
@@ -0,0 +1,16 @@
+class one{
+ void add(int a, int b) {
+     System.out.println(a+b);
+ }
+int add1(int a, int b) {
+return a+b;
+}
+public static void main(String[] args){
+one t1=new
+one();
+       t1.add(12,23);
+       t1.add(1,2);
+       int sum=t1.add1(10,20);
+System.out.println("Sum is: "+sum);
+  }
+}
